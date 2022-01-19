d9ac662f752ce20fd997d39702c0e36231d30933
Peter Washington <peter@wmkagency.com>
2022-01-19 14:28:34 -0700

    better type checking in Tag constructor

--- src/classes/Tag.ts
+++ src/classes/Tag.ts
@@ -19,8 +19,12 @@ export class Tag {
   name: string;
   slug: string;
   constructor(node: string | { name: string; slug: string }) {
-    const fromString = typeof node === "string" ? reshapeCtflTag(node) : null;
-    this.name = fromString ? fromString.name : node.name;
-    this.slug = fromString ? fromString.slug : node.slug;
+    if (typeof node === "string") {
+      this.name = reshapeCtflTag(node).name;
+      this.slug = reshapeCtflTag(node).slug;
+    } else {
+      this.name = node.name;
+      this.slug = node.slug;
+    }
   }
 }
